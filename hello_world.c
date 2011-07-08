6f83dc59b7bb89ba1fd873f480495854df66e88b
Shih-Yuan Lee (FourDollars) <fourdollars@gmail.com>
2011-07-08 23:31:31 +0800

    A simple C library.

---
+++ hello_world.c
@@ -0,0 +1,13 @@
+/* -*- coding: utf-8; indent-tabs-mode: nil; tab-width: 4; c-basic-offset: 4; -*- */
+/* vim:set fileencodings=utf-8 tabstop=4 expandtab shiftwidth=4 softtabstop=4: */
+
+/* hello_world.c */
+
+#include <stdio.h>
+#include "hello_world.h"
+
+int hello_world(void)
+{
+    return printf("Hello World!\n");
+}
+
