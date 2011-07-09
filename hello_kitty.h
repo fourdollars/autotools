ef0f1cd575ee6d248ea12eab88f0457a93b4e181
Shih-Yuan Lee (FourDollars) <fourdollars@gmail.com>
2011-07-09 18:34:29 +0800

    Add hello_kitty component.

--- hello_kitty.h
+++ hello_kitty.h
@@ -1,20 +1,20 @@
 /* -*- coding: utf-8; indent-tabs-mode: nil; tab-width: 4; c-basic-offset: 4; -*- */
 /* vim:set fileencodings=utf-8 tabstop=4 expandtab shiftwidth=4 softtabstop=4: */
 
-/* hello_world.h */
+/* hello_kitty.h */
 
-#ifndef __HELLO_WORLD__
-#define __HELLO_WORLD__
+#ifndef __HELLO_KIITY__
+#define __HELLO_KIITY__
 #ifdef __cplusplus
 extern "C"
 {
 #endif
     /** 
-     * @brief This is a hello world function.
+     * @brief This is a hello kitty function.
      * 
      * @return The reture value of printf.
      */
-    extern int hello_world(void);
+    extern int hello_kitty(void);
 #ifdef __cplusplus
 }
 #endif
