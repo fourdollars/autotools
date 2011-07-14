8c8838454bb57bc4c683724daeb9d5495332e418
Shih-Yuan Lee (FourDollars) <fourdollars@gmail.com>
2011-07-14 12:48:10 +0800

    Add autotools.

---
+++ 02_autotools/hello.c
@@ -0,0 +1,10 @@
+/* -*- coding: utf-8; indent-tabs-mode: nil; tab-width: 4; c-basic-offset: 4; -*- */
+/* vim:set fileencodings=utf-8 tabstop=4 expandtab shiftwidth=4 softtabstop=4: */
+
+#include <stdio.h>
+
+int main(int argc, char* argv[])
+{
+    printf("Hello World!\n");
+    return 0;
+}
