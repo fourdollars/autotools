447faa4d9c98b02715903988544024b69ef46286
Shih-Yuan Lee (FourDollars) <fourdollars@gmail.com>
2017-06-30 17:35:30 +0800

    Add the example for libtool versioning.

---
+++ 04_versioning/hello_kitty.c
@@ -0,0 +1,13 @@
+/* -*- coding: utf-8; indent-tabs-mode: nil; tab-width: 4; c-basic-offset: 4; -*- */
+/* vim:set fileencodings=utf-8 tabstop=4 expandtab shiftwidth=4 softtabstop=4: */
+
+/* hello_kitty.c */
+
+#include <stdio.h>
+#include "hello_kitty.h"
+
+int hello_kitty(void)
+{
+    return printf("Hello Kitty!\n");
+}
+
